f6fc9b34b93ae74f1d6a2e4d5e2cfeef1616f035
medhatk <medhatkaram76@gmail.com>
2026-06-20 22:57:30 +0300

    finall comit

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-/* popup cars*/
+/* popup cards*/
 const cards = document.querySelectorAll(".course-card");
 const popupOverlay = document.querySelector(".popup-overlay");
+const popupImage = document.getElementById("popup-image");
 const popupTitle = document.getElementById("popup-title");
 const popupDescription = document.getElementById("popup-description");
+const popupTeacher = document.getElementById("popup-teacher");
 const closeBtn = document.querySelector(".close-btn");
 
-cards.forEach(card => {
-    card.addEventListener("click", () => {
 
+cards.forEach(card => {
+    card.addEventListener("click", () => { 
         const title = card.querySelector("h3").textContent;
         const description = card.querySelector("p").textContent;
+        const image = card.querySelector("img").src;
+        const teacherName =card.querySelector(".teacher-name").textContent;
+        const teacherInfo =card.querySelector(".teacher-info").textContent;
 
+        popupImage.src = image;
         popupTitle.textContent = title;
-        popupDescription.textContent = description;
-
+        popupTeacher.textContent = teacherName;
+        popupDescription.textContent = teacherInfo;
         popupOverlay.classList.add("active");
     });
 });
@@ -45,4 +51,66 @@ menuBtn.addEventListener("click", () => {
         icon.classList.remove("fa-xmark");
         icon.classList.add("fa-bars");
     }
+});
+/* صفحه التسجيل */
+const enrollBtns = document.querySelectorAll(".Enroll-btn");
+
+const registerOverlay = document.querySelector(".register-overlay");
+const closeRegister = document.querySelector(".close-register");
+
+enrollBtns.forEach(btn => {
+
+    btn.addEventListener("click", () => {
+        registerOverlay.classList.add("active");
+    });
+
+});
+
+closeRegister.addEventListener("click", () => {
+    registerOverlay.classList.remove("active");
+});
+
+registerOverlay.addEventListener("click", (e) => {
+
+    if(e.target === registerOverlay){
+        registerOverlay.classList.remove("active");
+    }
+
+});
+/* الارسال للواتساب */
+const registerForm =
+document.getElementById("registerForm");
+
+registerForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const studentName =
+    registerForm.querySelector('[placeholder="اسم الطالب"]').value;
+
+    const phone =
+    registerForm.querySelector('[placeholder="رقم الهاتف"]').value;
+
+    const parentPhone =
+    registerForm.querySelector('[placeholder="رقم هاتف ولي الأمر"]').value;
+
+    const score =
+    registerForm.querySelector('[placeholder="المجموع من 280"]').value;
+
+    const track =
+    document.querySelector('input[name="track"]:checked')?.value;
+
+    const message = `
+اسم الطالب: ${studentName}
+رقم الهاتف: ${phone}
+رقم ولي الأمر: ${parentPhone}
+المجموع: ${score}/280
+المسار: ${track}
+`;
+
+    const whatsappUrl =
+`https://wa.me/201095378948?text=${encodeURIComponent(message)}`;
+
+    window.open(whatsappUrl, "_blank");
+
 });
